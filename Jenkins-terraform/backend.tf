59faf9ac08c2e070bfc9b4c4ead2c23c1fcb7f96
suresh01432 <mudavathsuresh0143@gmail.com>
2024-07-31 13:42:04 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "suresh2002" # Replace with your actual S3 bucket name
+    bucket = "suresh2025" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
